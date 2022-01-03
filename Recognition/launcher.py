4285769d63304f8ae9cb77fd5fea08059dbca256
hwrryss <hwrryss@gmail.com>
2022-01-03 13:52:33 +0300

    Delete Recognition directory

--- Recognition/launcher.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-
-while True:
-    now = datetime.now()
-
-    if int(now.strftime('%H')) < 22 and int(now.strftime('%H')) > 7:
-        os.system('python determinater.py')
