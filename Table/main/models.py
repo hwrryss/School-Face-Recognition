428fa14ee18f81f05ab90720c95f24231575d1db
hwrryss <hwrryss@gmail.com>
2022-06-11 18:09:54 +0300

    Re-do main page

--- Table/main/models.py
+++ Table/main/models.py
@@ -2,32 +2,39 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-
-
-class MainTable(models.Model):
+class Person(models.Model):
     class GradeChoice(models.TextChoices):
-        fifth_C = '5C', _('5C')
-        sixth_C = '6C', _('6C')
-        sixth_T = '6T', _('6T')
-        seventh_C = '7C', _('7C')
-        eighth_C = '8C', _('8C')
-        ninth_C = '9C', _('9C')
-        tenth_C = '10C', _('10C')
-        Teachers = 'Teachers', _('Teachers')
+        fifth_C = '5С', _('5С')
+        sixth_C = '6С', _('6С')
+        sixth_T = '6Т', _('6Т')
+        seventh_C = '7С', _('7С')
+        eighth_C = '8С', _('8С')
+        ninth_C = '9С', _('9С')
+        tenth_C = '10С', _('10С')
+        Teachers = 'Учитель', _('Учитель')
 
     class StatusChoice(models.TextChoices):
-        entered = 'Entered', _('Entered')
-        left = 'Left', _('Left')
+        entered = 'Вошел', _('Вошел')
+        left = 'Вышел', _('Вышел')
+        unknown = 'Неизвестно', _('Неизвестно')
+
+    class ReasonChoice(models.TextChoices):
+        unknown = 'Неизвестно', _('Неизвестно')
+        illness = 'Болеет', _('Болеет')
+        quarantine = 'Карантин', _('Карантин')
+        family = 'Семейные-обстоятельства', _('Семейные-обстоятельства')
+        other = 'Другое', _('Другое')
+
 
     grade = models.CharField(_('Grade'), max_length=50, choices=GradeChoice.choices, default=GradeChoice.fifth_C)
     name = models.CharField(_('LFM Names'), max_length=50)
     time = models.TimeField(_('Time'))
     status = models.CharField(_('Status'), max_length=50, choices=StatusChoice.choices, default=StatusChoice.entered)
+    reason = models.CharField(_('Reason'), max_length=50, choices=ReasonChoice.choices, default=ReasonChoice.unknown)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Main Table'
-        verbose_name_plural = 'Main Tables'
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
--- Table/main/views.py
+++ Table/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import MainTable
+from .models import Person
 
 from django.core import serializers
 from django.http import HttpResponse
@@ -13,26 +13,52 @@ from .check_requests import check_request
 import pandas as pd
 
 from . import rcModule
+from . import utils
 
+filt = '5С'
 
 def TablePage(request):
     context = {}
-    context['table'] = MainTable.objects.order_by('grade', 'name', 'time', 'status')
-
-    colors = []
+    context['table'] = Person.objects.order_by('grade', 'name', 'time', 'status')
+    context['colors'] = []
+    everyone = utils.parseStudents()
     table = context['table'].values()
     table = pd.DataFrame(table).to_dict(orient="list")
+    global filt
 
-    for status in table['status']:
-        colors.append('#E35C5C') if status == 'Left' else colors.append('#94E35C') #yellow - '#E3CF5C'
+    for entity in everyone:
+        if table:
+            if entity[1] not in table['name']:
+                Person.objects.create(grade = entity[0], name = ' '.join(entity[1:]), status = 'Неизвестно', time = '00:00:00', reason = 'Неизвестно')
+        else:
+            Person.objects.create(grade = entity[0], name = ' '.join(entity[1:]), status = 'Неизвестно', time = '00:00:00', reason = 'Неизвестно')
 
-    context['colors'] = colors
+    req = request.GET
 
-    if request.GET:
-        req = request.GET['grade']
-        context['table'].filter(name=req.split()[1]).update(grade=req.split()[0])
+    if req:
+        if 'reason' in req.keys():
+            req = request.GET['reason']
+            context['table'].filter(name=' '.join(req.split()[1:])).update(reason=req.split()[0])
+        else:
+            req = request.GET['filter']
+            filt = req
 
-    return render(request, 'main/MainTable.html', {'table': zip(context['table'], context['colors'])})
+    if table:
+        for status, reason in zip(table['status'], table['reason']):
+            if status == 'Вошел':
+                context['colors'].append('#A0E35C')
+
+            elif status == 'Неизвестно':
+                if reason == 'Неизвестно':
+                    context['colors'].append('#D3C85E')
+                else:
+                    context['colors'].append('E3B85C')
+
+            else:
+                context['colors'].append('#D3655E')
+
+
+    return render(request, 'main/MainTable.html', {'table': zip(context['table'], context['colors']), 'filter': filt} if context['table'] else {})
 
 
 def api(request):
@@ -41,14 +67,14 @@ def api(request):
     if 'jwt' in request.COOKIES.keys():
         if request.COOKIES['jwt'] == checkJWT:
             if request.COOKIES['action'] == 'report':
-                data = MainTable.objects.order_by('grade', 'name', 'time', 'status')
+                data = Person.objects.order_by('grade', 'name', 'time', 'status')
                 rcModule.createReport(data)
 
             if request.COOKIES['action'] == 'delete':
-                MainTable.objects.all().delete()
+                Person.objects.all().delete()
 
             if request.COOKIES['action'] == 'add':
-                table = MainTable.objects.order_by('grade', 'name', 'time', 'status')
+                table = Person.objects.order_by('grade', 'name', 'time', 'status')
 
                 data = json.loads(request.body.decode("utf-8"))
                 name = data["Name"]
@@ -57,6 +83,6 @@ def api(request):
                 status = data["Status"]
 
                 if check_request(table, name, status):
-                    MainTable.objects.bulk_create([MainTable(grade=grade, name=name, time=time, status=status)])
+                    Person.objects.bulk_create([Person(grade=grade, name=name, time=time, status=status)])
 
     return HttpResponse('<h1>API moment</h1>')
